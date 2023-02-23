63107bc04005b6b2db991905dfed86823450410f
iZer <zjokerunderzerz@gmail.com>
2023-02-23 11:46:47 +0700

    update code

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Initialize and add the map
+function initMap() {
+    // Your Location
+    const loc = { lat: 16.463859, lng: 107.6077933 };
+
+    // Centerd map on location
+    const map = new google.maps.Map(document.querySelector('.map'), {
+        center: loc,
+        zoom: 14
+    });
+
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Stick menu background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+// Smooth Scrolling
+$('#navbar a').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
