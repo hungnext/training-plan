3dd123d3a55fe10ad97fa4f3a8ad243d473c1730
hungnext <zjokerunderzerz@gmail.com>
2023-03-21 13:58:28 +0700

    update

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function initMap() {
 // Stick menu background
 window.addEventListener('scroll', function() {
     if (window.scrollY > 150) {
-        document.querySelector('#navbar').style.opacity = 0.7;
+        document.querySelector('#navbar').style.opacity = 0.9;
     } else {
         document.querySelector('#navbar').style.opacity = 1;
     }
